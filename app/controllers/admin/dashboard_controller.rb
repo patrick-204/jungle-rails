650a78e729cb69f20cda4db5786310e417507526
patrick-204 <p.ohara921@gmail.com>
2024-08-05 15:00:13 -0400

    Modify admin dashboard page to display count of products and categories in the db.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
   private
